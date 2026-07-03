5fb57b109eac56cde5647eae2f4174eb792edf90
Boluex <awomewekolawole1.com>
2026-07-03 18:01:55 +0100

    feat: update alert summary and add dynamic timestamps to Slack alert payload

--- alert.sh
+++ alert.sh
@@ -7,7 +7,8 @@ curl -X POST http://localhost:9093/api/v2/alerts \
       "namespace": "securebank"
     },
     "annotations": {
-      "summary": "Test alert from SecureBank",
-      "description": "This is a test alert to verify Slack integration"
-    }
+      "summary": "Test alert - Slack integration working!"
+    },
+    "startsAt": "'$(date -u +%Y-%m-%dT%H:%M:%SZ)'",
+    "endsAt": "'$(date -u -d "+10 minutes" +%Y-%m-%dT%H:%M:%SZ)'"
   }]'
